4c225ad76ba3161234ab6b5f878dd668e52eb8f7
Nomin <44207779+Nomin7711@users.noreply.github.com>
2025-02-01 17:51:48 -0600

    fix

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { BASE_URL } from '../config/api';
-import Loader from './Loader';
 
 const Login = () => {
     const navigate = useNavigate(); 
@@ -23,9 +22,10 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
+    setError("");
     try {
         const response = await axios.post(BASE_URL + "user/login", formData);
-        console.log(response);
         const { token } = response.data;
       localStorage.setItem('token', token);
         setLoading(false);
@@ -35,15 +35,17 @@ const Login = () => {
         setLoading(false);
         setError("Something went wrong. Please try again.");
       }
-    console.log(formData);
   };
 
   return (
     <div className="min-h-screen flex justify-center items-center bg-gray-100">
-        {loading && <Loader/>}
       <div className="w-full max-w-md p-8 bg-white shadow-lg rounded-lg">
-        
         <h2 className="text-2xl font-semibold text-center text-gray-800 mb-6">Log In</h2>
+        {error && 
+          <h3 className="text-red-500 text-sm font-medium text-center mt-2">
+            {error}
+          </h3>
+          }
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label htmlFor="email" className="block text-gray-700 font-medium">Email</label>
@@ -55,6 +57,7 @@ const Login = () => {
               onChange={handleChange}
               className="w-full p-3 border border-gray-300 rounded-lg focus:border-blue-500 focus:outline-none transition-colors"
               required
+              disabled={loading}
             />
           </div>
 
@@ -68,10 +71,21 @@ const Login = () => {
               onChange={handleChange}
               className="w-full p-3 border border-gray-300 rounded-lg focus:border-blue-500 focus:outline-none transition-colors"
               required
+              disabled={loading}
             />
           </div>
-
-          <button type="submit" className="w-full p-3 bg-blue-500 text-white rounded-lg cursor-pointer hover:bg-blue-700 transition-colors">Log In</button>
+          <button
+              type="submit"
+              className={`w-full p-3 ${loading ? 'bg-gray-500' : 'bg-blue-500 hover:bg-blue-700 transition-colors'} text-white rounded-lg cursor-pointer   flex items-center justify-center`}
+              disabled={loading}
+                        >
+              {loading && (
+                <svg className="mr-3 w-5 h-5 animate-spin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <circle cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" fill="none" strokeDasharray="31.4" strokeDashoffset="7.85" />
+                </svg>
+              )}
+              Log In 
+            </button>
         </form>
 
         <p className="mt-4 text-sm text-center text-gray-600">Don't have an account? <a href="/" className="text-blue-500 hover:underline">Sign up</a></p>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,21 +2,23 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { BASE_URL } from "../config/api";
-import Loader from "./Loader";
 
 const Signup = () => {
-    const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     name: "",
     email: "",
-    password: ""
+    password: "",
   });
   const [file, setFile] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState("");
   const [error, setError] = useState("");
+
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
   };
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
@@ -29,22 +31,33 @@ const Signup = () => {
     e.preventDefault();
     setLoading(true);
     setError("");
-    try {
-      const filename = encodeURIComponent(file.name);
-      const contentType = file.type;
-      
-      const response = await axios.post(BASE_URL + "user/signup", {...formData, profileImageName : filename, contentType});
+    setSuccess("");
 
-      const { uploadURL } = response.data;
-      if (!uploadURL) {
-        setError("Failed to get upload URL");
-        return;
+    try {
+      let requestData = { ...formData };
+
+      if (file) {
+        const filename = encodeURIComponent(file.name);
+        const contentType = file.type;
+        requestData.profileImageName = filename;  // Include the file name and content type in the request
+
+        const response = await axios.post(`${BASE_URL}user/signup`, requestData);
+
+        const { uploadURL } = response.data;
+        if (!uploadURL) {
+          setError("Failed to get upload URL");
+          return;
+        }
+
+        await axios.put(uploadURL, file, {
+          headers: { "Content-Type": contentType },
+        });
+      } else {
+        await axios.post(`${BASE_URL}user/signup`, requestData);
       }
-      const uploadResponse = await axios.put(uploadURL, file, {
-        headers: { "Content-Type": file.type }});
-      console.log(uploadResponse);
+
+      setSuccess("Signup successful! Please ");
       setLoading(false);
-      navigate("/login", { state: { message: "Signup successful! Please login using your email and password." } });
     } catch (err) {
       console.log(err);
       setLoading(false);
@@ -54,99 +67,113 @@ const Signup = () => {
 
   return (
     <div className="min-h-screen flex justify-center items-center bg-gray-100">
-      {loading ? (
-        <Loader />
-      ) : (
-        <div className="w-full max-w-md p-8 bg-white shadow-lg rounded-lg">
-          <h2 className="text-2xl font-semibold text-center text-gray-800 mb-6">
-            Sign Up
-          </h2>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <label htmlFor="name" className="block text-gray-700 font-medium">
-                Full Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                placeholder="Full name"
-                className="w-full p-3 border border-gray-300 rounded-lg focus:border-blue-500 focus:outline-none transition-colors"
-                required
-              />
-            </div>
-
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="block text-gray-700 font-medium"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                placeholder="Email"
-                className="w-full p-3 border border-gray-300 rounded-lg focus:border-blue-500 focus:outline-none transition-colors"
-                required
-              />
-            </div>
-
-            <div className="mb-4">
-              <label
-                htmlFor="password"
-                className="block text-gray-700 font-medium"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Password"
-                value={formData.password}
-                onChange={handleChange}
-                className="w-full p-3 border border-gray-300 rounded-lg focus:border-blue-500 focus:outline-none transition-colors"
-                required
-              />
-            </div>
-            <div className="mb-4">
-                <label
-                    htmlFor="password"
-                    className="block text-gray-700 font-medium"
-                >
-                    Profile picture
-                </label>
-                <div className="flex flex-col w-full  border border-gray-300 rounded-lg"> 
-                    <input
-                        type="file"
-                        onChange={handleFileChange}
-                        className=" file:bg-gray-200 file:text-black file:py-2 file:px-4 file:rounded-lg file:border-0"
-                    />
-                </div>
-            </div>
-           
-            <button
-              type="submit"
-              className="w-full p-3 bg-blue-500 text-white rounded-lg cursor-pointer hover:bg-blue-700 transition-colors"
-            >
-              Sign Up
-            </button>
-          </form>
-
-          <p className="mt-4 text-sm text-center text-gray-600">
-            Already have an account?{" "}
+      <div className="w-full max-w-md p-8 bg-white shadow-lg rounded-lg">
+        <h2 className="text-2xl font-semibold text-center text-gray-800 mb-6">
+          Sign Up
+        </h2>
+        {success && (
+          <h3 className="text-green-700 text-sm font-medium text-center mt-2">
+            {success}{" "}
             <a href="/login" className="text-blue-500 hover:underline">
-              Log in
+              Log in 
             </a>
-          </p>
-        </div>
-      )}
+          </h3>
+        )}
+        {error && (
+          <h3 className="text-red-500 text-sm font-medium text-center mt-2">
+            {error}
+          </h3>
+        )}
+
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-gray-700 font-medium">
+              Full Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              placeholder="Full name"
+              className="w-full p-3 border border-gray-300 rounded-lg focus:border-blue-500 focus:outline-none transition-colors"
+              required
+              disabled={loading}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700 font-medium">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Email"
+              className="w-full p-3 border border-gray-300 rounded-lg focus:border-blue-500 focus:outline-none transition-colors"
+              required
+              disabled={loading}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-700 font-medium">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
+              className="w-full p-3 border border-gray-300 rounded-lg focus:border-blue-500 focus:outline-none transition-colors"
+              required
+              disabled={loading}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="profileImage" className="block text-gray-700 font-medium">
+              Profile Picture
+            </label>
+            <div className="flex flex-col w-full border border-gray-300 rounded-lg">
+              <input
+                type="file"
+                disabled={loading}
+                onChange={handleFileChange}
+                className="file:bg-gray-200 file:text-black file:py-2 file:px-4 file:rounded-lg file:border-0"
+              />
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            className={`w-full p-3 ${
+              loading ? "bg-gray-500" : "bg-blue-500 hover:bg-blue-700 transition-colors"
+            } text-white rounded-lg cursor-pointer flex items-center justify-center`}
+            disabled={loading}
+          >
+            {loading && (
+              <svg className="mr-3 w-5 h-5 animate-spin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <circle cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" fill="none" strokeDasharray="31.4" strokeDashoffset="7.85" />
+              </svg>
+            )}
+            Sign Up
+          </button>
+        </form>
+
+        <p className="mt-4 text-sm text-center text-gray-600">
+          Already have an account?{" "}
+          <a href="/login" className="text-blue-500 hover:underline">
+            Log in
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
